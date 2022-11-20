f4a1b8f084fedc2a637899dc9c49810e9191db61
rgdevelopings <rahulgowlapalli01@gmail.com>
2022-11-20 16:45:17 +0530

    docker run

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,9 +5,7 @@ const {
   mongoConnect,
   mongoDisconnect,
 } = require('../../services/mongo');
-const {
-  loadPlanetsData 
-} = require('../../models/planets.model')
+
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
